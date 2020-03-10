e8a78feb2ceb7f35a9d728aae82f28ccbcad2c96
Leander Jehl <leanderjehl@yahoo.no>
2020-03-10 08:34:46 +0100

    fix

--- examples/js/vue3/fruits-router/src/router.js
+++ examples/js/vue3/fruits-router/src/router.js
@@ -2,8 +2,6 @@ let router = new VueRouter({
     routes: [
       { path: '/', component: myFavorites },
       { path: '/all', component: allFruits},
-      { path: '/student/:id', component: student, props: true}
 
     ]
 });
-let student = { props: ['id'], template: `<div>Student {{ id }}</div>`}
